bc530e379dd173e1c1099efffb7ea6a41ec7a6e9
Julien Rouaux <jn.rouaux@gmail.com>
2025-02-14 10:32:50 +0100

    kafka configuration

--- docker/kafka/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Démarrer Zookeeper
-$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
-sleep 5
-
-# Démarrer Kafka
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
